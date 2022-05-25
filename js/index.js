9a719490f0ecc647a01e2fe079174f05f0a9d5d0
RainbowHell <awper9794@gmail.com>
2022-05-25 16:37:53 +1000

    Notification --> Home Section [29:52]

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-let menu = document.querySelector('.menu-icon');
-let navbar = document.querySelector('.menu');
+let menuIcon = document.querySelector('.menu-icon');
+let notificationIcon = document.querySelector('#bell-icon');
 
-menu.onclick = () => {
-    menu.classList.toggle('move');
+let navbar = document.querySelector('.menu');
+let notification = document.querySelector('.notification');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('move');
     navbar.classList.toggle('active');
+    notification.classList.remove('active');
+}
+
+notificationIcon.onclick = () => {
+    notification.classList.toggle('active');
 }
