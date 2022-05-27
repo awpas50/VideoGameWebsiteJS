5c53c4a080947f8d885ebdc8d71114cc779b209c
RainbowHell <awper9794@gmail.com>
2022-05-27 15:47:06 +1000

    New Section [43:50]

--- js/index.js
+++ js/index.js
@@ -13,3 +13,31 @@ menuIcon.onclick = () => {
 notificationIcon.onclick = () => {
     notification.classList.toggle('active');
 }
+
+// Swiper script
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
