850081cb50cdb2e4ac8474b873050b4d05f81ded
awpas50 <awper9794@gmail.com>
2022-08-05 14:51:33 +1000

    Header / slider style modification

--- js/index.js
+++ js/index.js
@@ -5,49 +5,68 @@ let navbar = document.querySelector('.menu');
 let notification = document.querySelector('.notification');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('move');
-    navbar.classList.toggle('active');
-    notification.classList.remove('active');
+  menuIcon.classList.toggle('move');
+  navbar.classList.toggle('active');
+  notification.classList.remove('active');
 }
 
 notificationIcon.onclick = () => {
-    notification.classList.toggle('active');
+  notification.classList.toggle('active');
 }
 
 // Swiper script
-var swiper = new Swiper(".trending-content", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var swiper1 = new Swiper(".trending-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination-trending",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 15,
-      },
-      1068: {
-        slidesPerView: 4,
-        spaceBetween: 20,
-      },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 15,
     },
-  });
+    1068: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
 
-  // Custom Scroll Bar
-  window.onscroll = function() {myfunction()};
+var swiper2 = new Swiper(".home-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination-home",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
 
-  function myfunction() {
-    var windScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (windScroll / height) * 100;
-    document.getElementById('scroll-bar').style.width = scrolled + '%';
-  }
+// Custom Scroll Bar
+window.onscroll = function () { myfunction() };
+
+function myfunction() {
+  var windScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (windScroll / height) * 100;
+  document.getElementById('scroll-bar').style.width = scrolled + '%';
+}
